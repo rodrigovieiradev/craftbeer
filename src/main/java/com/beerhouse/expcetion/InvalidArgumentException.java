396b706ecb00f063827c0d7d4e20a2285c54d195
Rodrigo Vieira <rodrigo.v.info@gmail.com>
2021-01-30 18:14:33 -0300

    Criação da classe InvalidArgumentException.java

---
+++ src/main/java/com/beerhouse/expcetion/InvalidArgumentException.java
@@ -0,0 +1,14 @@
+package com.beerhouse.expcetion;
+
+public class InvalidArgumentException extends BeerHouseException {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidArgumentException() {
+		super();
+	}
+	
+	public InvalidArgumentException(String format, Object... args) {
+		super (String.format(format, args));
+	}
+}
